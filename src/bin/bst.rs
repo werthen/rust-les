ed8b966b9b0f7007d8c5faaf1b51225063d99de4
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-10-25 22:24:36 +0200

    add iterator for moving

--- src/bin/bst.rs
+++ src/bin/bst.rs
@@ -22,11 +22,29 @@ fn mutating() {
 
 fn moving() {
     let mut bs = moving::BST::<i32>::new();
-    for x in vec![3, 4, 5, 6] {
-        bs = bs.insert(x);
-        println!("{:?}", bs.root);
-    }
+
+    bs = bs.insert(6);
+    bs = bs.insert(3);
+    bs = bs.insert(2);
+    bs = bs.insert(1);
+    bs = bs.insert(4);
+    bs = bs.insert(5);
+    bs = bs.insert(8);
+    bs = bs.insert(7);
+
+    // .-8
+    // | `-7
+    // 6
+    // |   .-5
+    // | .-4
+    // `-3
+    //   `-2
+    //     `-1
 
     println!("{:?}", bs.contains(4));
     println!("{:?}", bs.contains(10));
+
+    for int in bs.into_iter() {
+        println!("{}", int);
+    }
 }
--- src/moving.rs
+++ src/moving.rs
@@ -1,14 +1,18 @@
 
-#[derive(Debug)]
-pub struct BST<T: Ord> {
-    pub root: Option<Box<Node<T>>>
-}
+use std::mem;
 
 #[derive(Debug)]
-pub struct Node<T: Ord> {
+pub struct BST<T> {
+    pub root: Edge<T>,
+}
+
+type Edge<T> = Option<Box<Node<T>>>;
+
+#[derive(Debug)]
+pub struct Node<T> {
     item: T,
-    left: Option<Box<Node<T>>>,
-    right: Option<Box<Node<T>>>,
+    left: Edge<T>,
+    right: Edge<T>,
 }
 
 impl<T: Ord> BST<T> {
@@ -66,3 +70,121 @@ impl<T: Ord> Node<T> {
         }
     }
 }
+
+pub struct InOrderIterator<T> {
+    remainder: Vec<Box<Node<T>>>,
+}
+
+
+impl<T> IntoIterator for BST<T> {
+    type Item = T;
+    type IntoIter = InOrderIterator<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        InOrderIterator {
+            remainder: self.root.into_iter().collect(),
+        }
+    }
+}
+
+impl<T> Iterator for InOrderIterator<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.remainder.pop().map(|mut node| {
+            while let Some(leftbox) = mem::replace(&mut node.left, None) {
+                self.remainder.push(node);
+                node = leftbox;
+            }
+            let unboxed = *node;
+            let Node { item, left: _, right} = unboxed;
+            if let Some(right) = right {
+                self.remainder.push(right);
+            }
+            item
+        })
+    }
+}
+
+// More complex, but perhaps useful for the mutating BST? That one should have an Iter (and an
+// IterMut), not a IntoIter.
+
+//
+// #[derive(PartialEq, Eq, Debug)]
+// pub enum Direction { Left, Right }
+// 
+// #[derive(Debug)]
+// pub struct InOrderIterator<T> {
+//     path:    Vec<(T, Direction, Edge<T>)>,
+//     subtree: Edge<T>,
+// }
+// 
+// impl<T> InOrderIterator<T> {
+//     // TODO none-public
+//     pub fn new(tree: BST<T>) -> Self {
+//         InOrderIterator {
+//             path:    Vec::new(),
+//             subtree: tree.root,
+//         }
+//     }
+// 
+//     pub fn go_left(&mut self) {
+//         if let Some(boxnode) = mem::replace(&mut self.subtree, None) {
+//             let node = *boxnode;
+//             let Node { item: i, left: l, right: r } = node;
+//             self.path.push((i, Direction::Left, r));
+//             self.subtree = l;
+//         }
+//     }
+// 
+//     pub fn go_right(&mut self) {
+//         if let Some(boxnode) = mem::replace(&mut self.subtree, None) {
+//             let node = *boxnode;
+//             let Node { item: i, left: l, right: r } = node;
+//             self.path.push((i, Direction::Right, l));
+//             self.subtree = r;
+//         }
+//     }
+// 
+//     pub fn go_leftmost(&mut self) {
+//         while self.subtree.is_some() {
+//             self.go_left();
+//         }
+//     }
+// 
+//     pub fn go_rightmost(&mut self) {
+//         while self.subtree.is_some() {
+//             self.go_right();
+//         }
+//     }
+// 
+//     pub fn go_up(&mut self) -> Option<Direction> {
+//         self.path.pop().map(|(item, direction, sidetree)| {
+//             let subtree = mem::replace(&mut self.subtree, None);
+//             let (lefttree, righttree) = match direction {
+//                 Direction::Left  => (subtree, sidetree),
+//                 Direction::Right => (sidetree, subtree),
+//             };
+//             self.subtree = Some(Box::new(Node {
+//                 item: item,
+//                 left: lefttree,
+//                 right: righttree,
+//             }));
+//             direction
+//         })
+//     }
+// }
+// 
+// impl<T> Iterator for InOrderIterator<T> {
+//     type Item = T;
+// 
+//     fn next(&mut self) -> Option<T> {
+//         match self.subtree {
+//             None => {
+//                 while Some(Direction::Left) == self.go_up() {}
+//                 self.subtree.map(|node| node.item)
+//             }, // go up left *, go up right, yield
+//             Some(ref node) => None, // go down right, go down left *, go up right, yield
+//         }
+//     }
+// }
