ac80ada1e1c7e3c51795e864c903b46366e9e137
Lorin Werthen <werthenlorin@gmail.com>
2016-10-26 16:06:17 +0200

    improve code

--- src/mutating.rs
+++ src/mutating.rs
@@ -1,41 +1,41 @@
 pub struct BST<T> {
-    pub root: Option<Box<Node<T>>>,
+    pub root: Edge<T>
 }
+
+type Edge<T> = Option<Box<Node<T>>>;
 
 #[derive(Debug, PartialEq)]
 pub struct Node<T> {
     val: T,
-    left: Option<Box<Node<T>>>,
-    right: Option<Box<Node<T>>>
+    left: Edge<T>,
+    right: Edge<T>
 }
 
 impl<T: Ord + Clone> BST<T> {
     pub fn new() -> Self {
-        BST { root: Option::None }
+        BST { root: None }
     }
 
     pub fn insert(&mut self, val: T) {
         match self.root {
-            None => {
-                self.root = Some(Box::new(Node {
-                    val: val,
-                    left: None,
-                    right: None,
-                }))
-            }
+            None => self.root = Some(Box::new(Node::new(val))),
             Some(ref mut node) => node.insert(val),
         }
     }
 
-    pub fn contains(&mut self, x: T) -> bool {
+    pub fn contains(&self, x: T) -> bool {
         match self.root {
             None => false,
-            Some(ref mut node) => node.contains(x),
+            Some(ref node) => node.contains(x),
         }
     }
 }
 
 impl<T: Ord> Node<T> {
+    pub fn new(val: T) -> Self {
+        Node { val: val, left: None, right: None }
+    }
+
     pub fn insert(&mut self, x: T) {
         if x == self.val {
             return
@@ -45,26 +45,20 @@ impl<T: Ord> Node<T> {
 
         match target {
             &mut Some(ref mut node) => node.insert(x),
-            &mut None => {
-                *target = Some(Box::new(Node {
-                    val: x,
-                    left: None,
-                    right: None,
-                }))
-            }
+            &mut None => *target = Some(Box::new(Node::new(x)))
         }
     }
 
-    pub fn contains(&mut self, x: T) -> bool {
+    pub fn contains(&self, x: T) -> bool {
         if self.val == x {
             return true
         }
 
-        let target = if self.val < x { &mut self.left } else { &mut self.right };
+        let target = if self.val < x { &self.left } else { &self.right };
 
-        match target {
-            &mut Some(ref mut subnode) => subnode.contains(x),
-            &mut None => false
+        match *target {
+            Some(ref subnode) => subnode.contains(x),
+            None => false
         }
     }
 }
